f3820cabab8c7c2a433f3291ec5db0095c37a053
Gutierrez PS <gutierrez@live.jp>
2017-04-19 11:38:42 -0300

    Removed NEC test, added blink LED

--- simple-ac-remote.ino
+++ simple-ac-remote.ino
@@ -10,27 +10,17 @@
 #include <IRremote.h>
 #include <IRremoteInt.h>
 
-// Defined in IRremoteInt.h
-extern int MATCH(int measured, int desired);
-extern int MATCH_MARK(int measured_ticks, int desired_us);
-extern int MATCH_SPACE(int measured_ticks, int desired_us);
-
-#define NEC_HDR_MARK    9000
-#define NEC_HDR_SPACE   4500
-#define NEC_BIT_MARK    560
-#define NEC_ONE_SPACE   1690
-#define NEC_ZERO_SPACE  560
-
 const struct {
     char button_level;
     char button_off;
     char led_1;
     char led_2;
     char led_3;
+    char led_blink;
     char ir_sensor;
-} g_pins = {12, 11, 4, 5, 6, 2};
+} g_pins = {12, 11, 4, 5, 6, 7, 2};
 
-unsigned int g_raw[4][100] = {{0}, {0}, {0}, {0}};
+unsigned int g_raw[4][100] = {{0}, {0}, {0}, {0}};      // stores raw timings
 char g_raw_size[4] = {0};
 
 IRrecv g_ir_recv(g_pins.ir_sensor);
@@ -125,6 +115,7 @@ void setup()
     pinMode(g_pins.led_1, OUTPUT);
     pinMode(g_pins.led_2, OUTPUT);
     pinMode(g_pins.led_3, OUTPUT);
+    pinMode(g_pins.led_blink, OUTPUT);
     pinMode(g_pins.button_off, INPUT_PULLUP);
     pinMode(g_pins.button_level, INPUT_PULLUP);
 
@@ -176,129 +167,11 @@ void loop()
                 break;
         }
 
+        digitalWrite(g_pins.led_blink, HIGH);
         g_ir_sender.sendRaw(g_raw[g_status], g_raw_size[g_status], 38);
-        delay(500);
+        delay(100);
+        digitalWrite(g_pins.led_blink, LOW);
+        delay(400);
         g_send = 0;
     }
 }
-
-
-/*
-
-
-bool decodeNECHVAC(decode_results *results, long *data0, long *data1) {
-    int offset = 1; // Skip initial space
-    int n_bits = 0;
-
-    // Initial mark
-    if (!MATCH_MARK(results->rawbuf[offset], NEC_HDR_MARK)) {
-        return false;
-    }
-    offset++;
-
-    // Initial space
-    if (!MATCH_SPACE(results->rawbuf[offset], NEC_HDR_SPACE)) {
-        return false;
-    }
-    offset++;
-
-    // ignore start space, header mark and space, and last mark
-    n_bits = (results->rawlen - 4)/2;
-    if(n_bits > 64) {
-        Serial.println("Overflow");
-        return false;
-    }
-
-    for (int i = 0; i < n_bits; i++) {
-        if (!MATCH_MARK(results->rawbuf[offset], NEC_BIT_MARK)) {
-            return false;
-        }
-        offset++;
-        if (MATCH_SPACE(results->rawbuf[offset], NEC_ONE_SPACE)) {
-            if(i < 32) {
-                *data0 = (*data0 << 1) | 1;
-            } else {
-                *data1 = (*data1 << 1) | 1;
-            }
-        } else if (MATCH_SPACE(results->rawbuf[offset], NEC_ZERO_SPACE)) {
-            if(i < 32) {
-                *data0 <<= 1;
-            } else {
-                *data1 <<= 1;
-            }
-        } else {
-            return false;
-        }
-        offset++;
-    }
-
-    return true;
-}
-
-
-void dump(decode_results *results)
-{
-    int count = results->rawlen;
-    Serial.print("Raw (");
-    Serial.print(count, DEC);
-    Serial.print("): ");
-    unsigned long space[2], mark[2];
-
-
-    for (int i = 1; i < count; i++) {
-        if (i & 1) {
-            Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
-        }
-        else {
-            Serial.write('-');
-            Serial.print((unsigned long) results->rawbuf[i]*USECPERTICK, DEC);
-        }
-        Serial.print(" ");
-    }
-    Serial.println();
-}
-
-
-void loop()
-{
-    if (g_ir_recv.decode(&results)) {
-        long data0 = 0, data1 = 0;
-        unsigned int raw_signal[100] = {0};
-        Serial.print("Detected - ");
-
-        
-        if(decodeNECHVAC(&results, &data0, &data1)) {
-            Serial.print("NEC ");
-            Serial.print(data0, HEX);
-            Serial.print("-");
-            Serial.println(data1, HEX);
-        } else {
-            dump(&results);
-        }
-        
-        
-        //Serial.println(results.value, HEX);
-        dump(&results);
-
-        for(unsigned char i = 1; i < results.rawlen; i++)
-        {
-            raw_signal[i-1] = results.rawbuf[i]*USECPERTICK;
-
-            Serial.print(raw_signal[i-1], DEC);
-            Serial.print(" ");
-        }
-        Serial.println(" ");
-
-        g_ir_recv.resume(); // Receive the next value
-
-        g_ir_sender.sendRaw(raw_signal, results.rawlen - 1, 38);
-
-        g_ir_recv.enableIRIn(); // Start the receiver
-
-        Serial.println("sent");
-    }
-
-    
-}
-
-*/
