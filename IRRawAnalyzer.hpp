f6c6910a8888470abac029b0d18cc2b17ca96017
Gutierrez PS <gutierrez@live.jp>
2017-07-16 14:26:55 -0300

    Analyzer registers mark and spaces differently

--- IRRawAnalyzer.hpp
+++ IRRawAnalyzer.hpp
@@ -5,7 +5,7 @@
 #include "IRData.hpp"
 #include "IRProtocols.hpp"
 
-#define MAX_DIFFERENT_VALUES 20
+#define MAX_DIFFERENT_VALUES 30
 
 void analyze(decode_results *results)
 {
@@ -14,21 +14,25 @@ void analyze(decode_results *results)
     uint8_t rawLength = results->rawlen;
     unsigned char i;
 
-    unsigned int values[MAX_DIFFERENT_VALUES] = {0};
-    unsigned char count[MAX_DIFFERENT_VALUES] = {0};
+    unsigned int values[MAX_DIFFERENT_VALUES] = {0};    // time width
+    unsigned char count[MAX_DIFFERENT_VALUES] = {0};    // number of occurrences
+    unsigned char polarity[MAX_DIFFERENT_VALUES] = {0}; // if it's a mark or space
     unsigned int value = 0;
     unsigned char occupied = 1;
+    unsigned char pol = 1;      // 1 for mark, 0 for space
 
     values[0] = results->rawbuf[offset];
     count[0] = 1;
+    polarity[0] = pol;  // first is a mark
 
     for(offset = 2; offset < rawLength; offset++)
     {
         value = results->rawbuf[offset];
+        pol = pol ? 0 : 1;      // flip polarity
 
         for(i = 0; i < occupied; i++)
         {
-            if(value == values[i])
+            if(value == values[i] && pol == polarity[i])
             {
                 count[i]++;
                 break;
@@ -39,7 +43,9 @@ void analyze(decode_results *results)
         {
             values[i] = value;
             count[i] = 1;
+            polarity[i] = pol;
             occupied++;
+
             if(occupied == MAX_DIFFERENT_VALUES)
             {
                 Serial.println("overflow");
@@ -50,6 +56,7 @@ void analyze(decode_results *results)
 
     for(i = 0; i < occupied; i++)
     {
+        Serial.print( polarity[i] ? '+' : '-' );
         Serial.print((unsigned long) values[i]*USECPERTICK, DEC);
         Serial.print(" ");
         Serial.println(count[i], DEC);
